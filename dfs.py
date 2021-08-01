0b0a696eddb952092690a31302d43dc8bbb16a99
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-01 19:24:00 -0400

    Depth-First Traversal

--- dfs.py
+++ dfs.py
@@ -3,14 +3,16 @@ def dfs(graph, current_vertex, target, visited=None):
     if visited == None:
         visited = []
     visited.append(current_vertex)
-    if current_vertex == target:
-        return visited 
-    else:
-        for neighbour in graph[current_vertex]:
-            if not neighbour in visited:
-                path = dfs(graph, neighbour, target, visited)
+    
+    for neighbour in graph[current_vertex]:
+        if not neighbour in visited:
+            path = dfs(graph, neighbour, target, visited)
 
-                if path:
-                    return path 
+            if path == None:
+                print(visited)
+                
+            
+    
+                
     
     
