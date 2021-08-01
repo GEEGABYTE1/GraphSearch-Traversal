b3825f5f3c4040fc15a4f6aa9b9583444c187383
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-01 19:02:39 -0400

    graph traversal using bfs

--- bfs.py
+++ bfs.py
@@ -4,18 +4,35 @@ def bfs(graph, start_vertex, target):
     vertex_and_path = [start_vertex, path]
     bfs_queue = [vertex_and_path]
     visited = set()
-    
+    paths = {}
+    counter = 0
+
     while bfs_queue:
+        print('-'*24)
         current_vertex, path = bfs_queue.pop()
         visited.add(current_vertex)
 
-        for neighbour in graph[start_vertex]:
+        print(path)
+        for neighbour in graph[current_vertex]:
             if not neighbour in visited:
                 if neighbour == target:
                     path.append(neighbour)
-                    return path 
+                    bfs_queue.append([neighbour, path])
+                    continue 
                 else:
                     path.append(neighbour)
                     bfs_queue.append([neighbour, path])
+        
+        paths[counter] = path
+        counter += 1
     
-    return None
+    #return paths 
+
+
+        
+    
+    
+            
+
+    
+
