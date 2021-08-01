03757c6216bb933da44967661b7cc536cf144f30
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-01 19:24:12 -0400

    DFS Traversal INputs

--- test.py
+++ test.py
@@ -43,18 +43,33 @@ for vertex, edge in test_graph.graph_dict.items():
 
 
 ### BFS Traversal ###
-
+'''
 first_element = list(test_graph.graph_dict.keys())[0]
 lst = list(test_graph.graph_dict[first_element].edges.keys())
 
 for i in range(len(lst)):
-    #first_edge = lst[0]
+    first_edge = lst[0]
     if i == 0:
         pass 
     else:
         dictionary[first_element].pop(0)
         lst.pop(0)
     output = bfs(dictionary, 1, 6)
+    '''
     
 #########################
+
+first_element = list(test_graph.graph_dict.keys())[0]
+lst = list(test_graph.graph_dict[first_element].edges.keys())
+
+for i in range(len(lst)):
+    first_edge = lst[0]
+    if i == 0:
+        pass
+    else:
+        dictionary[first_element].pop(0)
+        lst.pop(0)
+    
+    output = dfs(dictionary,1, 6)
+    print()
 
